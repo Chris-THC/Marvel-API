5fbad1729499f0930dffe6b1811b4d43287e2fce
Chris CR <chris_49git@outlook.com>
2024-11-15 15:27:32 -0600

    I create a new endpoint to get a character by ID

--- src/main/java/com/marvel/restController/MarvelRequestController.java
+++ src/main/java/com/marvel/restController/MarvelRequestController.java
@@ -5,6 +5,7 @@ import com.marvel.service.MarvelRequestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -28,5 +29,12 @@ public class MarvelRequestController {
     public ResponseEntity<List<MarvelRequest>> getAllMarvelRequestsLogs() {
         final List<MarvelRequest> MarvelLogList = marvelRequestService.getAllMarvelRequestsLogs();
         return ResponseEntity.ok().body(MarvelLogList);
+    }
+
+    @GetMapping(path = "/api/characters/{idCharacters}")
+    public Mono<ResponseEntity<String>> GetCharacterById(@PathVariable("idCharacters") final Integer idCharacters) throws Exception {
+        return marvelRequestService.getCharacterById(idCharacters)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
